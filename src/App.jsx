496072be089896e6fa00255042960bf8cbc1ecdb
AlejandroM12 <alejandro.morel@gmail.com>
2022-08-20 14:17:27 -0300

    Added connection with API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import { Container } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Searcher } from "./components/Searcher/Searcher";
+import { getGithubUser } from "./services/users";
+
  const App = () => {
+
   const [ inputUser, setInputUser ] = useState('octocat');
-  const [ userState, setUserState ] = useState('inputUser');
+  const [ userState, setUserState ] = useState(inputUser);
+  const [ notFound, setNotFound ] = useState(false);
+
+  const gettingUser = async (user) => {
+    const userResponse = await getGithubUser(user);
+
+    if(userState === 'octocat'){
+      localStorage.setItem('octocat', JSON.stringify(userResponse))
+    }
+    if(userResponse.message === 'Not Found'){
+      const { octocat } = localStorage;
+      setInputUser(octocat);
+      setNotFound(true);
+    }else{
+      setUserState(userResponse);
+      setNotFound(false);
+    }
+    console.log(userState)
+  }
+  useEffect(()=>{
+    gettingUser(inputUser);
+  },[inputUser]);
 
   return (
     <Container sx={{
